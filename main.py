77be60d3b319edaca85003e5c46c77e763cd28f2
AnissHal <anisshal50@gmail.com>
2021-01-26 20:13:06 +0100

    accessing all courses on moodle

--- main.py
+++ main.py
@@ -1,13 +1,103 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import webbrowser
+import questionary
 
-with urllib.request.urlopen('https://moodle.univ-chlef.dz/fr/course/index.php?categoryid=156') as fp:
+with urllib.request.urlopen('https://moodle.univ-chlef.dz/fr/') as fp:
+    soup = BeautifulSoup(fp.read(), 'html.parser')
+
+
+
+def get_category_names(soup):
+    faculty_choices = []
+    main_categroies = soup.select('h3.categoryname')
+    for d in main_categroies:
+        fac_names = d.select('a')[0].string
+        faculty_choices.append(fac_names + ' ->')
+    
+    return faculty_choices
+
+
+def get_category_links(soup):
+    main_categroies = soup.select('h3.categoryname')
+    faculty_links = []
+
+    for d in main_categroies:
+        links = d.select('a')[0]["href"]
+        faculty_links.append(links)
+
+    return faculty_links
+
+
+links = get_category_links(soup)
+choices = get_category_names(soup)
+
+
+choosen_fac = questionary.select(
+    "CHOOSE YOUR FACULTY",
+    choices=choices
+).ask()
+
+
+
+with urllib.request.urlopen(links[choices.index(choosen_fac)]) as fp:
+    soup = BeautifulSoup(fp.read(), 'html.parser')
+
+
+
+links = get_category_links(soup)
+choices = get_category_names(soup)
+
+
+choosen_fac = questionary.select(
+    "CHOOSE YOUR DEPERTEMENT",
+    choices=choices
+).ask()
+
+with urllib.request.urlopen(links[choices.index(choosen_fac)]) as fp:
     soup = BeautifulSoup(fp.read(), 'html.parser')
 
 courses = soup.find_all(attrs= {"data-courseid" : True})
 
-for course in courses:
-    course_name = course.select('h3.coursename')
-    course_link = course.select('a')
-    print(course_name[0].string)
-    print(course_link[0]['href'])
+
+links = get_category_links(soup)
+choices = get_category_names(soup)
+
+course_name = []
+course_link = []
+
+def get_courses(courses):
+    for course in courses:
+        try:
+            name = course.select('div.coursename')[0].string
+            link = course.select('div.coursename')[0].select('a')[0]['href']
+            course_link.append(link)
+        except IndexError:
+            name = course.select('h3.coursename')[0].string
+        course_name.append(name + ' [COURS]')
+
+
+
+get_courses(courses)
+
+
+choices.extend(course_name)
+
+choosen_lesson = questionary.select(
+"CHOOSE YOUR LEVEL",
+choices=choices
+).ask()
+
+if '->' in choosen_lesson:
+    course_name = []
+    course_link = []
+    get_courses(courses)
+    print(course_name)
+    choosen_lesson = questionary.select(
+    "CHOOSE YOUR COURSE",
+    choices=course_name
+    ).ask()
+    webbrowser.open(course_link[course_name.index(choosen_lesson)])
+else:
+    print(course_link[course_name.index(choosen_lesson)])
+    webbrowser.open(course_link[course_name.index(choosen_lesson)])
