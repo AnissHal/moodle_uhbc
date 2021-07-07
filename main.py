faeb66765d4e7e4183139437df4267de7a49a7a8
AnissHal <anisshal50@gmail.com>
2021-07-07 14:12:26 +0100

    moodle api

--- main.py
+++ main.py
@@ -1,117 +1,69 @@
-from bs4 import BeautifulSoup
-import urllib.request
-import webbrowser
-import questionary
-import time
-
-with urllib.request.urlopen('https://moodle.univ-chlef.dz/fr/') as fp:
-    soup = BeautifulSoup(fp.read(), 'html.parser')
+from typing import List
+from bs4 import BeautifulSoup as bs
+import urllib.request as request
+import json
 
 
+class Course:
+    def __init__(self, name, link) -> None:
+        self.name = name
+        self.link = link
 
-def get_category_names(soup):
-    faculty_choices = []
-    main_categroies = soup.select('h3.categoryname')
-    for d in main_categroies:
-        fac_names = d.select('a')[0].string
-        faculty_choices.append(fac_names + ' ->')
-    
-    return faculty_choices
+class MoodleApi:
+    def __init__(self) -> None:
+        self.__ids = []
 
+    def get_faculties(self) -> List:
+        with request.urlopen('https://moodle.univ-chlef.dz/fr') as fp:
+            self.soup = bs(fp.read(), 'html.parser')
+        faculties = []
+        elements = self.soup.select('h3.categoryname')
+        for element in elements:
+            fac_names = element.select('a')[0].string
+            link = element.select('a')[0]["href"]
+            faculties.append([fac_names, link])
 
-def get_category_links(soup):
-    main_categroies = soup.select('h3.categoryname')
-    faculty_links = []
+        return faculties
 
-    for d in main_categroies:
-        links = d.select('a')[0]["href"]
-        faculty_links.append(links)
+    def __search(self, arr, selector, n=0):
+        matches = []
+        for e in arr:
+            if selector[n] in e:
+                matches.append(e)
+        if n+1 == len(selector):
+            return matches[-1][-1]
+        return self.__search(matches, selector, n+1)
 
-    return faculty_links
+    def select(self, selector) -> 'MoodleApi':
+        """
+        select faculty or departement or level
+        
+        :param selector(str): selector
 
+        Returns:
+            MoodleApi: return self
+        """
+        if not self.__ids:
+            with open('id.txt', 'r') as f:
+                self.__ids = json.load(f)
+        self.__fetch(self.__search(self.__ids, selector.split('.')))
+        return self
 
-links = get_category_links(soup)
-choices = get_category_names(soup)
+    def __fetch(self, id) -> None:
+        with request.urlopen('https://moodle.univ-chlef.dz/fr/course/index.php?categoryid={}&perpage=100'.format(id)) as fp:
+            self.soup = bs(fp.read(), 'html.parser')
 
+    def get_courses(self) -> List:
+        """
+        return list of courses
 
-choosen_fac = questionary.select(
-    "CHOOSE YOUR FACULTY",
-    choices=choices
-).ask()
-
-
-
-with urllib.request.urlopen(links[choices.index(choosen_fac)]) as fp:
-    soup = BeautifulSoup(fp.read(), 'html.parser')
-
-
-
-links = get_category_links(soup)
-choices = get_category_names(soup)
-
-
-choosen_fac = questionary.select(
-    "CHOOSE YOUR DEPERTEMENT",
-    choices=choices
-).ask()
-
-with urllib.request.urlopen(links[choices.index(choosen_fac)]) as fp:
-    soup = BeautifulSoup(fp.read(), 'html.parser')
-
-courses = soup.find_all(attrs= {"data-courseid" : True})
-
-
-links = get_category_links(soup)
-choices = get_category_names(soup)
-
-course_name = []
-course_link = []
-
-def get_courses(courses):
-    for course in courses:
-        try:
-            name = course.select('div.coursename')[0].string
-            link = course.select('div.coursename')[0].select('a')[0]['href']
-        except IndexError:
-            name = course.select('h3.coursename')[0].string  
-            link = course.select('h3.coursename')[0].select('a')[0]['href']
-        course_name.append(name + ' [COURS]')
-        course_link.append(link)
-
-
-
-get_courses(courses)
-
-choices.extend(course_name)
-if choices:
-    choosen_lesson = questionary.select(
-    "CHOOSE YOUR LEVEL",
-    choices=choices
-    ).ask()
-
-else: 
-    print('NO LESSONS YET')
-    time.sleep(2)
-    exit()
-
-if '->' in choosen_lesson:
-    with urllib.request.urlopen(links[choices.index(choosen_lesson)]) as fp:
-        soup = BeautifulSoup(fp.read(), 'html.parser')
-
-    courses = soup.find_all(attrs= {"data-courseid" : True})
-
-    course_name = []
-    course_link = []
-    get_courses(courses)
-
-    course_name = []
-    course_link = []
-    get_courses(courses)
-    choosen_lesson = questionary.select(
-    "CHOOSE YOUR COURSE",
-    choices=course_name
-    ).ask()
-    webbrowser.open(course_link[course_name.index(choosen_lesson)])
-else:
-    print(course_link[course_name.index(choosen_lesson)])
-    webbrowser.open(course_link[course_name.index(choosen_lesson)])
+        Returns:
+            List: list containing all courses
+        """
+        courses = []
+        elements = self.soup.find_all(attrs={"data-courseid": True})
+        for element in elements:
+            name = element.select('.coursename')[0].string
+            link = element.select('.coursename')[0].select('a')[0]['href']
+            courses.append(Course(name, link))
+        return courses
